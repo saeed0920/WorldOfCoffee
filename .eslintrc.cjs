42032492b8240a53bdf1791f3f4f81f90d30b2ac
Saeed0920 <sd.eed1381@gmail.com>
2024-07-27 18:53:27 +0330

    feat:Add eslint , fix: configs of prettierrc and vscode_settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react", "@typescript-eslint", "prettier", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
   },
-}
+};
