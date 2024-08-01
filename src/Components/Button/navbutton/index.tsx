4166a5dea5dbdae65f3cc824f7392a0fed7a0d7b
saeed0920 <sd.eed1381@gmail.com>
2024-08-01 12:45:47 +0330

    Fix:some bugs

--- src/Components/Button/navbutton/index.tsx
+++ src/Components/Button/navbutton/index.tsx
@@ -1,8 +1,11 @@
-
-const navbutton=(text:string)=>{
-    return(
-        <button style={{backgroundColor:'#F9C06A'}} className="whitespace-nowrap	   hover:bg-yellow-700   text-center rounded-full w-24 font-semibold p-3 text-md playfair text-black ">{text}</button>
-    ) 
-
-}
-export default navbutton
+const navbutton = (text: string) => {
+  return (
+    <button
+      style={{ backgroundColor: "#F9C06A" }}
+      className="whitespace-nowrap	   hover:bg-yellow-700   text-center rounded-full w-24 font-semibold p-3 text-md playfair text-black "
+    >
+      {text}
+    </button>
+  );
+};
+export default navbutton;
