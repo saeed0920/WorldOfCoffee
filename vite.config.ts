bff57e0b26329b404647ebdcacb089d237c425fc
saeed0920 <42695081+saeed0920@users.noreply.github.com>
2024-08-18 22:56:35 +0330

    Setup server (#11)
    * feat: Add i18n ,responsive for phone and tablet ,Use i18n in all components fix:some styles
    
    * fix : prettier ingore for pnpm-lock
    
    * feat: Add data and package call server ,config .env file and vite.config.ts
    
    * fix:conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
 import eslint from "vite-plugin-eslint";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
-
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  plugins: [react(), eslint()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
-      "@public": fileURLToPath(new URL("./public", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
     },
-  },
+    define: {
+      "process.env": JSON.stringify(env),
+    },
+    plugins: [react(), eslint()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
+        "@public": fileURLToPath(new URL("./public", import.meta.url)),
+      },
+    },
+  };
 });
