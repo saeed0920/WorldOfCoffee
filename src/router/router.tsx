7891df48a7f647cb62cd8e07efc30349af2151b9
Reza NezhadSalari <reza.9salari6@gmail.com>
2024-08-22 03:20:13 +0330

    fetching data

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,8 @@ import Home from "@/views/home/Home";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    //we have many routs probebly so it shoul be in routs
+
     <Route path="/" element={<Home />} errorElement={<div>Ops!</div>}>
       {/* Child Elements */}
     </Route>,
--- src/views/Coffee's/Cappuccino.tsx
+++ src/views/Coffee's/Cappuccino.tsx
@@ -1,12 +1,28 @@
+import { useCoffee } from "@/Contexts/CoffeesContext";
 import Cappu from "@public/Images/Cappu.jpg";
 //This Compo it might be updated So... tell me then change it -_-
 function Cappuccino() {
+  const { coffeeList } = useCoffee();
+
+  console.log(coffeeList);
+
+  if (!coffeeList) {
+    return <p>Loading coffee data...</p>;
+  }
+
+  if (coffeeList.length === 0) {
+    return <p>No coffee data available.</p>;
+  }
+
+  const randomIndex = Math.floor(Math.random() * coffeeList.length);
+  const randomCoffee = coffeeList[randomIndex];
+
   return (
     <div className="flex flex-col border-2 border-coffee-brown pb-14" style={{ backgroundColor: "wheat" }}>
       <p className="text-center playfair">Discribe Coffee</p>
       <img src={Cappu} />
-      <p className="text-center text-coffee-brown playfair text-2xl py-3 bold">Cappuccino</p>
-      <p className="text-center playfair text-l"> Discrabe the coffee </p>
+      <p className="text-center text-coffee-brown playfair text-2xl py-3 bold">{randomCoffee.name}</p>
+      <p className="text-center playfair text-l">{randomCoffee.description}</p>
     </div>
   );
 }
