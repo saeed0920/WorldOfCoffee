3bb363e20f96f7caba22fd10c2e857b6c42332b2
Reza <reza.9salari6@gmail.com>
2024-09-15 05:25:23 +0330

    fetching data (#12)
    * fetching data
    
    * fix:bugs in import router ,fix fetch api call for other component
    
    * feat:"Chenging folders name"
    
    * editing
    
    ---------
    
    Co-authored-by: saeed0920 <sd.eed1381@gmail.com>

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,11 @@
 import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
-import Home from "@/views/home/Home";
+import Home from "@/Views/Home/Home";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    //we have many routs probebly so it shoul be in routs
+
+    // <Route index at='home' errorElement={<div>Ops!</div>}>
     <Route path="/" element={<Home />} errorElement={<div>Ops!</div>}>
       {/* Child Elements */}
     </Route>,
