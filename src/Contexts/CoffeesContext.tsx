8db583072d99ccfce16d3f45db44d16f79787e90
saeed0920 <sd.eed1381@gmail.com>
2024-09-18 02:28:46 +0330

    fix:add dependency in useEffect in conffecontext component

--- src/Contexts/CoffeesContext.tsx
+++ src/Contexts/CoffeesContext.tsx
@@ -12,32 +12,35 @@ function CoffeeProvider({ children }: InferProps<typeof CoffeeProvider.propTypes
   const { t } = useTranslation();
   const toast = useToast();
 
-  useEffect(function () {
-    function fetchCoffeeData() {
-      fetch(`${BASE_URL}/coffees`)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("Network response was not ok"); // Throw an error if the response is not ok
-          }
-          return res.json();
-        })
-        .then((data: Coffee[]) => {
-          setCoffeeList(data);
-        })
-        .catch((error) => {
-          console.log(error);
-          toast({
-            title: t("error_api_title"),
-            description: t("error_api_describe"),
-            status: "error",
-            duration: 3000,
-            isClosable: true,
-            position: "top-right",
+  useEffect(
+    function () {
+      function fetchCoffeeData() {
+        fetch(`${BASE_URL}/coffees`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error("Network response was not ok"); // Throw an error if the response is not ok
+            }
+            return res.json();
+          })
+          .then((data: Coffee[]) => {
+            setCoffeeList(data);
+          })
+          .catch((error) => {
+            console.log(error);
+            toast({
+              title: t("error_api_title"),
+              description: t("error_api_describe"),
+              status: "error",
+              duration: 3000,
+              isClosable: true,
+              position: "top-right",
+            });
           });
-        });
-    }
-    fetchCoffeeData();
-  }, []);
+      }
+      fetchCoffeeData();
+    },
+    [t, toast],
+  );
 
   return <CoffeeContext.Provider value={coffeeList}>{children}</CoffeeContext.Provider>;
 }
