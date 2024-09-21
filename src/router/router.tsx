151d815955754ac1bcf6db841a6faf8d343402d3
Ali <ali.abbasi.1382.shiraz@gmail.com>
2024-09-21 13:01:40 +0330

    feat: some minors changes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,10 @@
 import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
 import Home from "@/Views/Home/Home";
-import notfound from "../Views/NotFound";
+import Notfound from "@/Views/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />} errorElement={notfound()}>
+    <Route path="/" element={<Home />} errorElement={<Notfound/>}>
      
      
     </Route>,
