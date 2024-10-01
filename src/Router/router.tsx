0e8736075d149217f035e71bcb20532ba4010a03
aliabbasiS <135698286+aliabbasiS@users.noreply.github.com>
2024-10-01 15:51:11 +0330

    feat: change router and fix redirecting / to /main (#15)

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,14 +1,10 @@
-import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
-import Home from "@/Views/Home/Home";
-import Notfound from "@/Views/NotFound";
+import Home from "@/Views/Home/Home"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />} errorElement={<Notfound/>}>
-     
-     
-    </Route>,
-  ),
-) as ReturnType<typeof createBrowserRouter>;
 
-export default router;
+const routes=[
+    {path:'/main',element:Home},
+    
+    
+    
+]
+export default routes
